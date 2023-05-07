d442bb92df910dc43c65137085fed4e869f9c04a
twgituser <ec2-user@ip-172-31-21-177.ec2.internal>
2023-05-07 10:08:19 +0000

    updated docker run, included tag and port mapping

--- run_docker.sh
+++ run_docker.sh
@@ -2,11 +2,11 @@
 
 ## Complete the following steps to get Docker running locally
 
-# Step 1:
-# Build image and add a descriptive tag
+# Step 1: Build image and add a descriptive tag
+docker build --tag sklearn_predictor .
 
-# Step 2: 
-# List docker images
+# Step 2: List docker images
+docker image list
 
-# Step 3: 
-# Run flask app
+# Step 3: Run flask app
+docker run --publish 8000:80 sklearn_predictor
